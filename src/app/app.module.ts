53ad8fb703fde9344a14ad9bfe0207c4776837f8
Marvin Wichmann <marvin.wichmann@unic.com>
2019-02-21 13:47:05 +0100

    Added storefront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StorefrontModule } from '@spartacus/storefront';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    StorefrontModule.withConfig({
+      server: {
+        baseUrl: 'https://localhost:9002',
+        occPrefix: '/rest/v2/'
+      },
+      authentication: {
+        client_id: 'mobile_android',
+        client_secret: 'secret'
+      }
+    }),
     AppRoutingModule
   ],
   providers: [],
